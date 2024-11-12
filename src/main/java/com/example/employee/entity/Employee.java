25ada2bf405497148223fc54c469a6127e01c0f6
BhargaviG98 <bhargavigangula98@gmail.com>
2024-11-12 16:08:42 +0530

    Update Employee.java

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -19,7 +19,7 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+//Hii
     @NotNull(message = "name should not be not null")
     @Size(min =2,max=50,message="name characters should in the middle of 2 to 50" )
     private String name;
